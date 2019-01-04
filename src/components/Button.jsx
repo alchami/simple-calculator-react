fe1b6c57a2b6d2ba48da6d4b4ccb83a38a6f606b
alchami <alice.chaminas@riseup.net>
2019-01-04 10:25:14 +0100

    Declaring a function in parent Component make passing a function works

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, {Component} from "react";
 import PropTypes from "prop-types";
+
+// Should stay stateless
+/*class Button extends Component {
+    render() {
+        const label   = this.props.label;
+        const command = this.props.command;
+
+        return(<button id={label} className="btn btn-dark" onClick={command}>{label}</button>)
+    }
+}*/
 
 const Button = ({label, command}) => (
     <button id={label} className="btn btn-dark" onClick={command}>{label}</button>
 );
-
 Button.propTypes = {
     label: PropTypes.string.isRequired,
-    command: PropTypes.string.isRequired
+    command: PropTypes.func.isRequired
 };
 
 export default Button;
--- src/components/ButtonsContainer.jsx
+++ src/components/ButtonsContainer.jsx
@@ -8,7 +8,7 @@ class ButtonsContainer extends Component {
     }
 
     render() {
-        const test = console.log("pressed!");
+        const test = () => console.log("pressed!");
         return(
             <div id="calculator-buttons">
                 <Button label="0" command={test}/>
